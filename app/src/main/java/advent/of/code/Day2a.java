ed62773666f9aee100644bb93a4ff175323aa239
Ernest Derry <ernest.derry@travisperkins.co.uk>
2025-12-03 23:02:23 +0000

    day2a

---
+++ app/src/main/java/advent/of/code/Day2a.java
@@ -0,0 +1,15 @@
+package advent.of.code;
+
+import java.util.List;
+
+public class Day2a extends Solution {
+
+    Day2a() {
+        super.day = 2;
+        super.part = "a";
+    }
+
+    String solve(List<String> puzzleInput) {
+        return "";
+    }
+}
