800ff8122469a6c8e133a06b628c75d430f3151b
Ernest Derry <ernest.derry@travisperkins.co.uk>
2025-12-16 11:50:49 +0000

    day5a setup

---
+++ app/src/main/java/advent/of/code/Day5a.java
@@ -0,0 +1,10 @@
+package advent.of.code;
+
+import java.util.List;
+
+public class Day5a {
+
+    String solve(List<String> puzzleInput) {
+        return "";
+    }
+}
---
+++ app/src/test/java/advent/of/code/Day5aTest.java
@@ -0,0 +1,44 @@
+package advent.of.code;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Day5aTest {
+
+    Day5a day5a;
+
+    @BeforeEach
+    public void setup() {
+        day5a = new Day5a();
+    }
+
+    // @Test
+    // public void examplePuzzleInput() throws IOException {
+    //     List<String> puzzleInput = readPuzzleInputFromFile("day5_example.txt"); 
+    //     assertEquals("", day5a.solve(puzzleInput));
+    // }
+
+    // @Test
+    // public void actualPuzzleInput() throws IOException {
+    //     List<String> puzzleInput = readPuzzleInputFromFile("dayX.txt"); 
+    //     assertEquals("", dayX.solve(puzzleInput));
+    // }
+
+    private List<String> readPuzzleInputFromFile(String filename) throws FileNotFoundException, IOException {
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filename)) { 
+            if (inputStream == null) {
+                throw new FileNotFoundException("Resource file not found!");
+            }
+            return new BufferedReader(new InputStreamReader(inputStream)).lines().toList(); 
+        }
+    }
+}
