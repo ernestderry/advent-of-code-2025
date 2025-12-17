ca239e22301089101a1ec366bd12599f39d7caf6
Ernest Derry <ernest.derry@travisperkins.co.uk>
2025-12-17 16:55:52 +0000

    day5a done

--- app/src/main/java/advent/of/code/Day5a.java
+++ app/src/main/java/advent/of/code/Day5a.java
@@ -5,6 +5,32 @@ import java.util.List;
 public class Day5a {
 
     String solve(List<String> puzzleInput) {
-        return "";
+
+        int delimeterPos = puzzleInput.indexOf("");
+        List<String> freshRanges = puzzleInput.subList(0, delimeterPos);
+
+        List<Range> ranges = freshRanges.stream()
+            .map(range -> {
+                String[] rangeElements = range.split("-");
+                long min = Long.parseLong(rangeElements[0]);
+                long max = Long.parseLong(rangeElements[1]);
+                return new Range(min, max);
+            }).toList();
+
+        List<String> ids = puzzleInput.subList(delimeterPos + 1, puzzleInput.size());
+
+        long freshTotal =  ids.stream()
+            .filter(id -> inRange(id, ranges))
+            .count();        
+
+        return String.valueOf(freshTotal);
     }
+
+    boolean inRange(String id, List<Range> ranges) {
+        long idLong = Long.parseLong(id);
+        return ranges.stream()
+            .anyMatch(range -> idLong >= range.min && idLong <= range.max);
+    }
+
+    record Range(long min, long max) {};
 }
--- app/src/test/java/advent/of/code/Day5aTest.java
+++ app/src/test/java/advent/of/code/Day5aTest.java
@@ -21,17 +21,41 @@ public class Day5aTest {
         day5a = new Day5a();
     }
 
-    // @Test
-    // public void examplePuzzleInput() throws IOException {
-    //     List<String> puzzleInput = readPuzzleInputFromFile("day5_example.txt"); 
-    //     assertEquals("", day5a.solve(puzzleInput));
-    // }
+    @Test
+    public void oneRangeAndIdNotInRange() {
+        List<String> puzzleInput = List.of("1-3","","4");
+        assertEquals("0", day5a.solve(puzzleInput));        
+    }
 
-    // @Test
-    // public void actualPuzzleInput() throws IOException {
-    //     List<String> puzzleInput = readPuzzleInputFromFile("dayX.txt"); 
-    //     assertEquals("", dayX.solve(puzzleInput));
-    // }
+    @Test
+    public void oneRangeAndIdInRange() {
+        List<String> puzzleInput = List.of("1-3","","2");
+        assertEquals("1", day5a.solve(puzzleInput));        
+    }
+
+    @Test
+    public void oneRangeAndMultipleIdsInRange() {
+        List<String> puzzleInput = List.of("1-3","","1","2","3","4");
+        assertEquals("3", day5a.solve(puzzleInput));        
+    }
+
+    @Test
+    public void twoRangesAndMultipleIdsInRanges() {
+        List<String> puzzleInput = List.of("1-30","20-60","","1","29","30","100");
+        assertEquals("3", day5a.solve(puzzleInput));        
+    }
+
+    @Test
+    public void examplePuzzleInput() throws IOException {
+        List<String> puzzleInput = readPuzzleInputFromFile("day5_example.txt"); 
+        assertEquals("3", day5a.solve(puzzleInput));
+    }
+
+    @Test
+    public void actualPuzzleInput() throws IOException {
+        List<String> puzzleInput = readPuzzleInputFromFile("day5.txt"); 
+        assertEquals("679", day5a.solve(puzzleInput));
+    }
 
     private List<String> readPuzzleInputFromFile(String filename) throws FileNotFoundException, IOException {
         try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filename)) { 
